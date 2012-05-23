09544e11bc2e7048f2ff4f7cef16ec0d3beb46d0
Lauri Piispanen <lauri.piispanen@conmio.com>
2012-05-23 11:34:49 +0300

    updated plugin descriptor

--- ClosureCompilerResourcesGrailsPlugin.groovy
+++ ClosureCompilerResourcesGrailsPlugin.groovy
@@ -1,6 +1,6 @@
-class YuiMinifyResourcesGrailsPlugin {
+class ClosureCompilerResourcesGrailsPlugin {
   // the plugin version
-  def version = "0.1.5"
+  def version = "0.1"
   // the version or versions of Grails the plugin is designed for
   def grailsVersion = "1.2 > *"
   // the other plugins this plugin depends on
@@ -12,19 +12,18 @@ class YuiMinifyResourcesGrailsPlugin {
       "web-app/js/**/*.*"
   ]
 
-  def title = 'YUI Minify Resources'
-  def description = 'Minifies static css and js resources using the YUI Compressor library'
+  def title = 'Google Closure Resources'
+  def description = 'Passes js resources through the Google Closure Compiler'
   def documentation = 'http://grails.org/plugin/yui-minify-resources'
 
-  def author = 'Carsten Block'
-  def authorEmail = 'info@block-consult.com'
+  def author = 'Conmio Ltd'
+  def authorEmail = 'info@conmio.com'
   def license = '''
 The plugin is licensed under Apache 2 license, see http://www.apache.org/licenses/LICENSE-2.0
-The YUI Compressor library that is used to compress the resources is licensed under BSD license, see http://developer.yahoo.com/yui/license.html
 '''
 
-  def organization = [name: "Block Consult", url: "http://www.block-consult.com"]
-  def scm = [url: "https://github.com/cblock/yui-minify-resources"]
+  def organization = [name: "Conmio Ltd", url: "http://www.conmio.com"]
+  def scm = [url: "https://github.com/conmio/google-closure-compiler-resources"]
 
   def doWithWebDescriptor = {}
   def doWithSpring = {}
