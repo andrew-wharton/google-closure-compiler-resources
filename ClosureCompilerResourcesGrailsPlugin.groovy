03260141ed9b4f2f591831cc28ecf8fa20f515f3
Lauri Piispanen <lauri@piispanen.fi>
2012-08-24 11:16:57 +0300

    Improved plugin descriptor

--- ClosureCompilerResourcesGrailsPlugin.groovy
+++ ClosureCompilerResourcesGrailsPlugin.groovy
@@ -13,23 +13,15 @@ class ClosureCompilerResourcesGrailsPlugin {
   ]
 
   def title = 'Google Closure Resources'
-  def description = 'Passes js resources through the Google Closure Compiler'
-  def documentation = 'http://grails.org/plugin/yui-minify-resources'
+  def description = 'Compresses JavaScript resources using Google Closure Compiler'
+  def documentation = 'https://github.com/conmio/google-closure-compiler-resources'
 
   def author = 'Conmio Ltd'
   def authorEmail = 'info@conmio.com'
-  def license = '''
-The plugin is licensed under Apache 2 license, see http://www.apache.org/licenses/LICENSE-2.0
-'''
+  def license = 'APACHE'
   def issueManagement = [ system: "github", url: "https://github.com/conmio/google-closure-compiler-resources/issues" ]
 
   def organization = [name: "Conmio Ltd", url: "http://www.conmio.com"]
   def scm = [url: "https://github.com/conmio/google-closure-compiler-resources"]
 
-  def doWithWebDescriptor = {}
-  def doWithSpring = {}
-  def doWithDynamicMethods = {}
-  def doWithApplicationContext = {}
-  def onChange = {}
-  def onConfigChange = {}
 }
